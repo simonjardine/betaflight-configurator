aaae49909fc627b3ef131d13a93b96a4a2e13bcb
Simon Jardaine <simonjardine@gmail.com>
2026-03-24 11:05:38 +1000

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/betaflight-configurator/sw.js', { scope: '/betaflight-configurator/' })})}
